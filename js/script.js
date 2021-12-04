308e54b4fe70ff383c4051ed13b9f668b40a78cb
Vitor Souza <vitorhugovhps@outlook.com>
2021-12-04 11:34:43 -0300

    hover effect in JS is working

--- js/script.js
+++ js/script.js
@@ -21,3 +21,36 @@ function makeGrid(number){
 }
 
 makeGrid(gridNumber);
+
+
+const items = document.querySelectorAll(".grid-item");
+
+items.forEach((item) => {
+    item.addEventListener('mouseover', () => {
+        item.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+        
+    });
+});
+
+const button = document.querySelector("button");
+
+button.addEventListener('click', () => {
+    let newSize = prompt("What is the size of grid?");
+    gridNumber = parseInt(newSize);
+
+    while (container.firstChild){
+        container.removeChild(container.lastChild);
+    }
+    
+    makeGrid(gridNumber);
+
+    const items = document.querySelectorAll(".grid-item");
+
+    items.forEach((item) => {
+    item.addEventListener('mouseover', () => {
+        item.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+        
+        });
+    });
+});
+
